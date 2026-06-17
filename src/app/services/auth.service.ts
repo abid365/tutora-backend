efcdd5992b819d492919900df470f83196c583e9
abid365 <abidibnazam@gmail.com>
2026-06-17 16:42:48 +0600

    wip:otp verification

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,8 +5,19 @@ import { users } from "../../db/schema/user.schema.js";
 
 class ApiError extends Error {
   status: number;
-  constructor(message: string, status: number) {
+  error: any;
+  constructor(message: string, status: number, error: any) {
     super(message);
+    this.status = status;
+    this.error = error;
+  }
+}
+
+class Success {
+  status: number;
+  message: string;
+  constructor(message: string, status: number) {
+    this.message = message;
     this.status = status;
   }
 }
@@ -15,13 +26,21 @@ export const registerUser = async (data: RegisterInput) => {
   const { phone, email, password, name } = data;
   const hashedPassword = await argon2.hash(password);
   try {
-    const response = await db.insert(users).values({
-      name: name,
-      email: email,
-      phone: phone,
-      password: hashedPassword,
-    });
+    // todo: create user acc after phone number verification via otp
+    const response = await db
+      .insert(users)
+      .values({
+        name: name,
+        email: email,
+        phone: phone,
+        password: hashedPassword,
+      })
+      .returning();
+
+    if (response.length > 0) {
+      return new Success("User registered successfully", 201);
+    }
   } catch (error) {
-    throw new Error();
+    throw new ApiError("Failed to register user", 499, error);
   }
 };
